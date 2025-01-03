bd836a614768fa71ade620c26d7e6a7cc1e6289b
Zhiwei Zhang <Zhiwei.Zhang@amlogic.com>
2025-01-03 17:12:11 +0800

    using strerror to handle standard error code

--- zzwlib/custom_error.hpp
+++ zzwlib/custom_error.hpp
@@ -3,21 +3,30 @@
 #include <string>
 #include <string_view>
 
-#include "base.hpp"
-
 namespace zzwlib {
     class custom_error {
 
     public:
         enum class type : int {
+            ok = 0,
             base_custom_error = 1000,
         };
-
-        static std::string_view to_string(type err) noexcept {
-            if (err < type::base_custom_error) {
-                return base_err::to_string(err);
+        static int to_positive(int err) noexcept {
+            if (err < 0) {
+                return -err;
             }
-            switch (err) {
+            return err;
+        }
+        static std::string_view to_string(type err) noexcept {
+            int positive_err_code = to_positive(static_cast<int>(err));
+
+            if (positive_err_code < type::base_custom_error) {
+                return strerror(static_cast<int>(err));
+            }
+
+            switch (positive_err_code) {
+            case type::ok:
+                return "ok";
             case type::base_custom_error:
                 return "base_custom_error";
             }
